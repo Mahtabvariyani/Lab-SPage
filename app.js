9bf9f1680f830df249a9777a8d67582b37298495
Mah <mahtabvariyani@gmail.com>
2023-08-29 16:52:10 +0200

    Lab

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3001;
+
+// Serve static files from the "public" folder (if you have any)
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Set up routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+
